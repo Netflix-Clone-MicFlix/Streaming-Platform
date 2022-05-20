dfe7396bf4a07fbd9374a8ca5497f13bd437ab82
Mickey krekels <42765197+MickeyKrekels@users.noreply.github.com>
2022-05-20 14:22:00 +0200

    update environment files

--- src/app/environments/services.environment.ts
+++ src/app/environments/services.environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
-    production: false,
-    movieService: 'https://movie-service/v1/movie',
-    userService: 'https://user-service/v1/user',
-    keycloakUrl: 'https://local-keycloak/auth'
-  };
+  production: false,
+  movieService: 'https://localhost/movie-service/v1/movie',
+  userService: 'https://localhost/user-service/v1/user',
+  keycloakUrl: 'https://localhost/auth',
+};
