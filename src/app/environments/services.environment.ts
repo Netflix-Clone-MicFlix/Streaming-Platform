989a51ba330ebcb7046dfb38add062fe97784991
Mickey krekels <42765197+MickeyKrekels@users.noreply.github.com>
2022-06-12 15:12:54 +0200

    Update services.environment.ts

--- src/app/environments/services.environment.ts
+++ src/app/environments/services.environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  movieService: 'http://data.krekels-server.com/v1/movie',
-  userService: 'http://data.krekels-server.com/v1/user',
+  movieService: 'http://data.krekels-server.com/movie-service/v1/movie',
+  userService: 'http://data.krekels-server.com/user-service/v1/user',
   keycloakUrl: 'http://keycloak.krekels-server.com/auth',
 };
